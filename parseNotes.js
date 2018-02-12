111794738a2ffafca0c53998fa6141f1ce445258
Dawid Dominiak <dawid@swingdev.io>
2018-02-12 11:25:49 +0100

    Remove global from parse regexp

--- parseNotes.js
+++ parseNotes.js
@@ -1,9 +1,10 @@
 const moment = require('moment');
+const os = require('os');
 
 const PROJECT_ID_REGEXP = /\[[0-9]+\]/g;
 const TAG_REGEXP = /#\w+/g;
-const HOURS_REGEXP = /^\s*([0-9]{1,2}):([0-9]{2})\s*-\s*([0-9]{1,2}):([0-9]{2})\s*/g;
-const EMPTY_LINE_REGEXP = /^\s*$/g;
+const HOURS_REGEXP = /^\s*([0-9]{1,2}):([0-9]{2})\s*-\s*([0-9]{1,2}):([0-9]{2})\s*/;
+const EMPTY_LINE_REGEXP = /^\s*$/;
 function findProjectId(line) {
   const projectId = line
     .match(PROJECT_ID_REGEXP)
@@ -37,10 +38,10 @@ function findStartAndDuration(line, lastDate) {
 }
 
 function parseNotes(notes) {
-  const lines = notes.split('\n');
+  const lines = notes.split(os.EOL);
   let lastDate = null;
   return lines.reduce((timeEntries, line) => {
-    const isDate = /^\s*[0-9]{1,2}\.[0-9]{2}\.[0-9]{4}\s*$/g.test(line);
+    const isDate = /^\s*[0-9]{1,2}\.[0-9]{2}\.[0-9]{4}\s*$/.test(line);
     const isEntry = HOURS_REGEXP.test(line);
     const isEmptyLine = EMPTY_LINE_REGEXP.test(line);
 
